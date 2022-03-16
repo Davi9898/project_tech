9f5de33069b335c3823114f66495c65dab535820
Davi9898 <Danielvink1998@gmail.com>
2022-03-16 17:58:56 +0100

    Commit met alle back-end

---
+++ app.js
@@ -0,0 +1,61 @@
+require('dotenv').config()
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport')
+
+const app = express();
+
+
+// Passport config
+require('./config/passport')(passport);
+
+//Database
+
+const db = process.env.MONGODB;
+
+// Connecting to MongoDB
+mongoose.connect(db, { useNewUrlParser: true})
+    .then(() => console.log('MONGODB connected...'))
+    .catch(err => console.log(err));
+
+// EJS 
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// BodyParser
+app.use(express.urlencoded({ extended: false}));
+
+//Express session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect Flash
+app.use(flash());
+
+// Globale variabelen om met flash messages te requesten
+app.use(function(req, res, next) {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+  });
+
+
+//Routes die nodig zijn
+app.use('/', require('./routes/index'));
+
+app.use('/users', require('./routes/users'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+const User = require('../models/User');
+
+// Welcome Page
+router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+  User.find( { }, (error, users) => {
+    res.render('dashboard', {
+      user: req.user,
+      users: users
+    })
+  })  
+);
+
+module.exports = router;
